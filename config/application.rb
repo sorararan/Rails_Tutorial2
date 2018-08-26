e1252a86e7aa6ac96aedacbec12b6f777b00d343
sorararan <masolainota@gmail.com>
2018-08-27 01:40:56 +0900

    日本語化、日本時間化

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,10 @@ module RailsTutorial2
     # the framework and any gems in your application.
 
     config.action_view.embed_authenticity_token_in_remote_forms = true
+    #日本時間にする
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    #日本語化について
+    config.i18n.default_locale = :ja
   end
 end
